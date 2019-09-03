27fdb707bab687452bea7e1115e09d96193b72b3
Roman <kustyrt@gmail.com>
2019-09-03 08:31:38 +0400

    Disable SSL certificate verification

--- src/CurlHttpClient.php
+++ src/CurlHttpClient.php
@@ -9,6 +9,9 @@ class CurlHttpClient implements HttpClientInterface
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         $resp = curl_exec($ch);
         return $resp;
     }
